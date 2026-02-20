f3837090e49f3a1470b7af79513e09cebad1d1f1
lellivalentina1992-lang <lelli.valentina1992@gmail.com>
2026-02-20 18:23:20 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,20 @@
-const CACHE_NAME = 'lettore-offline-v1';
+const CACHE_NAME = 'scanner-gold-v1';
 const RISORSE = [
     './',
     './index.html',
-    'https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js'
+    './manifest.json',
+    'https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js',
+    'https://cdn-icons-png.flaticon.com/512/7075/7075315.png'
 ];
 
-// Installazione: salva i file in cache
 self.addEventListener('install', (e) => {
     e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(RISORSE);
-        })
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(RISORSE))
     );
 });
 
-// Intercettazione: se non c'è internet, usa la cache
 self.addEventListener('fetch', (e) => {
     e.respondWith(
-        caches.match(e.request).then((res) => {
-            return res || fetch(e.request);
-        })
+        caches.match(e.request).then((res) => res || fetch(e.request))
     );
 });
